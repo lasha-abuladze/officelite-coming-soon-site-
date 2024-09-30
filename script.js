aa3e4c5c6a070350f8ed70ab83b65a4dc02f2088
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2024-09-30 19:49:43 +0400

    add inner page

---
+++ script.js
@@ -0,0 +1,15 @@
+
+`use strict`;
+
+
+const btn = document.querySelector(`.btn--get-started`);
+
+btn?.addEventListener(`click`, () => {
+
+    // console.log(`hi`)
+    window.open("./secondpage.html", "_blank");
+    // window.location.href = "./secondpage.html";
+})
+
+
+// console.log(`hi`)
